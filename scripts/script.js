390fc0b5b938d387bd10ab81005f7b65bec45862
LauraButkute <laura.butkut@gmail.com>
2020-11-23 10:43:56 +0200

    progress

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@
 const search = document.getElementById('search'),
     submit = document.getElementById('submit'),
     drinks = document.getElementById('drink-name')
-    description = document.getElementById('drink-description')
-    img = document.getElementById('drink-image');
+    resultHeading = document.getElementById('result-heading')
+    single_drink = document.getElementById('single-drink');
 
 
 
@@ -12,20 +12,27 @@ const search = document.getElementById('search'),
 function searchDrink(e) {
     e.preventDefault();
 
+    single_drink.innerHTML = '';
+
     const term = search.value;
 
     if(term.trim()) {
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`)
             .then(res => res.json())
             .then(data => {console.log(data)
-            drinks.innerHTML = `Results for '${term}':`
+            resultHeading.innerHTML = `<h2>Results for '${term}': </h2>`
 
             if(data.drinks === null) {
-                drinks.innerHTML = `No results for '${term}'`
+                drinks.innerHTML = `
+                    <div class='no-result'><img src="/assets/2.jpg"></div>`;
+                resultHeading.innerHTML = `<h2>No results for '${term}'</h2>`
             } else {
-                description.innerHTML = data.drinks.map (drink =>
-                    `<div class='drink'>
+                drinks.innerHTML = data.drinks.map (drink =>`
+                    <div class='drink'>
                         <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}"/>
+                        <div class="drink-info" data-drinkID="${drink.idDrink}">
+                            <h3>${drink.strDrink}</h3>
+                        </div>
                     </div>`
                 )
                 .join('');
@@ -34,10 +41,66 @@ function searchDrink(e) {
         // CLEAR SEARCH AREA
         search.value = '';
     } else {
-        alert('Enter search name');
+        alert('Please fill the search area');
     }
+}
+// GETTING INFO OF COCKTAIL ON CLICK
+function getDrinkInfo(drinkID) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkID}`)
+        .then(res => res.json())
+        .then(data => {
+            const drink = data.drinks[0];
 
-};
-// event listener
+            addDrinkToDOM(drink);
+        });
+}
+
+// ADD DRINKS TO DOM
+function addDrinkToDOM(drink) {
+    const ingredients = [];
+
+    for (let i = 1; i < 15; i++) {
+        if(drink[`strIngredient${i}`]) {
+            ingredients.push(`${drink[`strIngredient${i}`]} - ${drink[`strMeasure${i}`]}`)
+        } else {
+            break;
+        }
+    }
+    
+    single_drink.innerHTML = `
+        <div class='single-drink'>
+            <h1>${drink.strDrink}</h1>
+            <img src='${drink.strDrinkThumb}' alt='${drink.strDrink}'/>
+            <div class='single-drink-info'>
+                ${drink.strIBA ? `<p>${drink.strIBA}</p>` : ''}
+                ${drink.strGlass ? `<p>${drink.strGlass}</p>` : ''}
+            </div>
+            <div class='main'>
+                <p>${drink.strInstructions}</p>
+                <h2>Ingredients</h2>
+                    <ul>
+                        ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                    </ul>
+            </div>
+        </div>
+    `;
+}
+
+// EVENT LISTENERS
 
 submit.addEventListener('submit', searchDrink);
+
+drinks.addEventListener('click', e => {
+    const drinkInfo = e.path.find(item => {
+        if(item.classList) {
+            return item.classList.contains('drink-info')
+        } else {
+            return false;
+        }
+    });
+    
+    if(drinkInfo) {
+        const drinkID = drinkInfo.getAttribute('data-drinkid');
+        getDrinkInfo(drinkID);
+    }
+});
---
+++ scripts/scriptTime.js
@@ -0,0 +1,44 @@
+function realTimeClock() {
+    let rtClock = new Date();
+
+    let hours = rtClock.getHours();
+    let minutes = rtClock.getMinutes();
+    let seconds = rtClock.getSeconds();
+
+    // AM PM
+    let amPm = (hours < 12) ? "AM" : "PM"
+
+    // 12 hour format
+    hours = (hours > 12) ? hours - 12 : hours;
+
+    // Add zeros in front of hours minutes seconds
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    // Display
+    document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds + " " + amPm;
+
+    setTimeout(realTimeClock, 1000);
+}
+
+    // Set background and greeting 
+function setBackground () {    
+
+    let today = new Date ();
+    let hour = today.getHours();
+
+    if(hour < 12) {
+        // morning
+        document.body.style.backgroundColor = '#f2f2f2'
+    } else if (hour < 18) {
+        // afternoon
+        document.body.style.backgroundColor = '#f0dfdf';
+    } else {
+        // evening
+        document.body.style.backgroundColor = '#ddd'
+    }
+}
+    // run
+
+    realTimeClock();
+    setBackground ();
